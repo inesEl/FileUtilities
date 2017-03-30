8f6bb417668bfc94e33416dd633c557399edd97c
Ines El Euch <ieuch@vistaprint.com>
2017-03-30 17:11:16 +0100

    Initializing repo

---
+++ FileUtilities-Lib/BasicFileUtilities.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileUtilities_Lib
+{
+    public class BasicFileUtilities
+    {
+        /// <summary>
+        /// Add content to a file.
+        /// This method deletes existing content in a file and place new content.
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="content"></param>
+        public static void AddContent(string path, string content)
+        {
+            File.WriteAllText(path, content);
+        }
+
+        /// <summary>
+        /// Add content to a file.
+        /// This method appends the content to existing content of the file.
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="content"></param>
+        public static void AppendContent(string path, string content)
+        {
+            File.AppendAllText(path, content);
+        }
+
+        public static bool CreateFile(string path, bool overwriteIfExists = false)
+        {
+            var fileCreated = false;
+            if ((File.Exists(path) && overwriteIfExists) || !File.Exists(path))
+            {
+                var stream = File.Create(path);
+                stream.Close();
+                fileCreated = true;
+            }
+            return fileCreated;
+        }
+
+        public static bool DeleteFile(string path)
+        {
+            File.Delete(path);
+            return !File.Exists(path);
+        }
+
+        public static string[] ReadFileAsArrayOfStrings(string path)
+        {
+            return File.ReadAllLines(path);
+        }
+
+        public static string ReadFileAsString(string path)
+        {
+            return File.ReadAllText(path);
+        }
+    }
+}
---
+++ Test-FileUtilitiesLib/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FileUtilities_Lib;
+
+namespace Test_FileUtilitiesLib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"D:\hello_world.txt";
+
+            // Create the file
+            BasicFileUtilities.CreateFile(path);
+
+
+            // Add content
+            var content = "Hello World\n";
+            BasicFileUtilities.AddContent(path, content);
+
+
+            // Read the file & display in the console
+            Console.WriteLine("First content added to the file : \n");
+            Console.WriteLine(BasicFileUtilities.ReadFileAsString(path));
+            Console.WriteLine("Added content to the file : \n");
+
+
+            // Append some more content
+            var username = "Hello, John Doe !\n";
+            BasicFileUtilities.AppendContent(path, username);
+
+
+            // Read the file & display in the console
+            foreach (var line in BasicFileUtilities.ReadFileAsArrayOfStrings(path))
+            {
+                Console.WriteLine(line);
+            }
+
+
+            // Delete the file
+            //var isFileDeleted = BasicFileUtilities.DeleteFile(path);
+            //var msg = isFileDeleted ? "deleted successfully" : "not deleted";
+            //Console.WriteLine("The file is "+ msg);
+        }
+    }
+}
